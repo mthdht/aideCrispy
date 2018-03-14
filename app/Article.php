8df709a9b1b258223bb8b1a5ae3a824ee64307cc
mthdht <mthdht@gmail.com>
2018-03-14 09:16:06 +0100

    create the article model

---
+++ app/Article.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+
+    /**
+     * Get the category that belong the article
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo('App\Article');
+    }
+}
