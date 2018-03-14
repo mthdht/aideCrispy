c574f085257247f9bfa5f7d50da289f7ff64d6e9
mthdht <mthdht@gmail.com>
2018-03-14 10:38:00 +0100

    made the category and article migration

---
+++ database/migrations/2018_03_14_084906_create_articles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->unique();
+            $table->text('content');
+            $table->integer('category_id')->unsigned();
+            $table->timestamps();
+
+            //foreign key
+            $table->foreign('category_id')
+                ->references('id')->on('categories')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
