5209ae54ec3d00113cce7a3960abc975c5f657b2
mthdht <mthdht@gmail.com>
2018-03-14 11:40:54 +0100

    made first version of welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,20 +13,41 @@
         <link rel="stylesheet" href="{{ asset('css/w3.css') }}">
     </head>
     <body>
-        <div class="w3-bar w3-blue-gray w3-padding">
-            <div class="w3-content">
-                <a href="javascript:void(0)" class="w3-bar-item w3-button w3-left w3-hide-large w3-hide-medium" onclick="toggleNav()">&#9776;</a>
-                <a href="/" class="w3-bar-item w3-button w3-hover-none w3-hover-text-blue">Blog de Crispy</a>
-                <a href="categories" class="w3-bar-item w3-hide-small w3-button w3-hover-none w3-hover-text-blue w3-right">Catégories</a>
-                <a href="articles" class="w3-bar-item w3-hide-small w3-button w3-hover-none w3-hover-text-blue w3-right">Articles</a>
+        <nav>
+            <div class="w3-bar w3-blue-gray w3-padding">
+                <div class="w3-content">
+                    <a href="javascript:void(0)" class="w3-bar-item w3-button w3-left w3-hide-large w3-hide-medium" onclick="toggleNav()">&#9776;</a>
+                    <a href="/" class="w3-bar-item w3-button w3-hover-none w3-hover-text-blue">Blog de Crispy</a>
+                    <a href="categories" class="w3-bar-item w3-hide-small w3-button w3-hover-none w3-hover-text-blue w3-right">Catégories</a>
+                    <a href="articles" class="w3-bar-item w3-hide-small w3-button w3-hover-none w3-hover-text-blue w3-right">Articles</a>
+                </div>
             </div>
-        </div>
 
-        <div class="w3-sidebar w3-light-gray w3-bar-block w3-card w3-animate-left" style="width:200px;display:none;" id="sidebar">
-            <a href="articles" class="w3-bar-item w3-button">Articles</a>
-            <a href="categories" class="w3-bar-item w3-button">Catégories</a>
-        </div>
+            <div class="w3-sidebar w3-light-gray w3-bar-block w3-card w3-animate-left" style="width:200px;display:none;" id="sidebar">
+                <a href="articles" class="w3-bar-item w3-button">Articles</a>
+                <a href="categories" class="w3-bar-item w3-button">Catégories</a>
+            </div>
+        </nav>
 
+        <div class="w3-main">
+            <div class="w3-content">
+                @foreach($articles as $article)
+                    <div class="w3-card w3-light-gray w3-margin ">
+                        <h5 class="title w3-teal w3-padding w3-container">
+                            {{ $article->title }}
+                        </h5>
+                        <div class="content w3-padding w3-container">
+                            {{ substr($article->content, 0, 200) }} ...
+                        </div>
+                        <div class="category w3-padding w3-container w3-blue-gray">
+                            Categorie: {{ $article->category->title }}
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+
+
+        </div>
 
         <!-- javascript -->
         <script>
