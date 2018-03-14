7894e0030716378c8246b85aa5c9208105d76820
mthdht <mthdht@gmail.com>
2018-03-14 11:40:33 +0100

    correct category method on article controller

--- app/Article.php
+++ app/Article.php
@@ -21,6 +21,6 @@ class Article extends Model
      */
     public function category()
     {
-        return $this->belongsTo('App\Article');
+        return $this->belongsTo('App\Category');
     }
 }
